042e54fe86bae30d14ec25652ac68a7267744a30
Copons <Copons@users.noreply.github.com>
2015-11-24 17:20:48 +0100

    Navbar handling (WIP)

--- dev/js/gg-framework.js
+++ dev/js/gg-framework.js
@@ -1,24 +1,55 @@
 ;(function($){
 
+// CLOSE EVERYTHING ON CLICK OUTSIDE
+
+$(document).on('click', function (e) {
+  if (!$(e.target).closest('.navbar-dropdown').length) {
+    closeNavbarDropdown($('.navbar-dropdown').next());
+  }
+});
+
+
+
+// Navbar dropdowns handling
 
 $('.navbar-dropdown').on('click', function (e) {
   e.preventDefault();
+  closeNavbarDropdown($('.navbar-dropdown').not($(this)).next());
+
   var navbarItem = $(this).next();
+
+  if (navbarItem.css('display') === 'none') {
+    openNavbarDropdown(navbarItem);
+  }
+  else if (navbarItem.css('display') === 'block') {
+    closeNavbarDropdown(navbarItem);
+  }
+});
+
+
+
+
+
+
+function openNavbarDropdown (navbarItem) {
   var options = {
-    duration: 400,
+    duration: 300,
     easing: [0.25, 0.8, 0.25, 1],
     queue: false
   };
-  if (navbarItem.css('display') === 'none') {
-    navbarItem
-      .velocity('fadeIn', options)
-      .velocity('slideDown', options);
-  }
-  else if (navbarItem.css('display') === 'block') {
-    navbarItem
+  navbarItem
+    .velocity('fadeIn', options)
+    .velocity('slideDown', options);
+}
+function closeNavbarDropdown (navbarItem) {
+  var options = {
+    duration: 300,
+    easing: [0.25, 0.8, 0.25, 1],
+    queue: false
+  };
+  navbarItem
     .velocity('fadeOut', options)
     .velocity('slideUp', options);
-  }
-});
+}
 
 })(Zepto);
